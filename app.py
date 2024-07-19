07839c19e239376bad1d77e063c8f4f2a8f5a0dc
Riv33R <dimageht69@gmail.com>
2024-07-20 02:18:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from datetime import datetime
 
 from flask import Flask, render_template, request, redirect, url_for
 
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 
 from flask_wtf import FlaskForm
 
@@ -15,14 +22,14 @@ import csv
 
 app = Flask(__name__)
 
-app.secret_key = 'your_secret_key'
+app.secret_key = "your_secret_key"
 
 
 login_manager = LoginManager()
 
 login_manager.init_app(app)
 
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 
 
 class User(UserMixin):
@@ -33,7 +40,6 @@ class User(UserMixin):
 
 
 @login_manager.user_loader
-
 def load_user(user_id):
 
     return User(user_id)
@@ -41,11 +47,11 @@ def load_user(user_id):
 
 class LoginForm(FlaskForm):
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
 
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
 
-    submit = SubmitField('Login')
+    submit = SubmitField("Login")
 
 
 def read_csv_data(filepath, search_term):
@@ -54,31 +60,37 @@ def read_csv_data(filepath, search_term):
 
     try:
 
-        with open(filepath, newline='', encoding='utf-8') as csvfile:
+        with open(filepath, newline="", encoding="utf-8") as csvfile:
 
             reader = csv.DictReader(csvfile)
 
             for row in reader:
 
-                if search_term.lower() in row.get('UserName', '').lower() or search_term.lower() in row.get('ComputerName', '').lower():
+                if (
+                    search_term.lower() in row.get("UserName", "").lower()
+                    or search_term.lower() in row.get("ComputerName", "").lower()
+                ):
 
                     # Convert LogonTime string to datetime object
 
-                    logon_time = datetime.strptime(row['LogonTime'], '%y-%m-%d %H:%M')
+                    logon_time = datetime.strptime(row["LogonTime"], "%y-%m-%d %H:%M")
 
                     # Format the datetime object to the desired format
 
-                    row['LogonTime'] = logon_time.strftime('%d.%m.%Y %H:%M')
+                    row["LogonTime"] = logon_time.strftime("%d.%m.%Y %H:%M")
 
                     results.append(row)
 
             # Sort results by datetime objects in LogonTime
 
-            results.sort(key=lambda x: datetime.strptime(x['LogonTime'], '%d.%m.%Y %H:%M'), reverse=True)
+            results.sort(
+                key=lambda x: datetime.strptime(x["LogonTime"], "%d.%m.%Y %H:%M"),
+                reverse=True,
+            )
 
     except FileNotFoundError:
 
-        results = [{'Error': 'File not found or cannot be accessed'}]
+        results = [{"Error": "File not found or cannot be accessed"}]
 
     except ValueError as e:
 
@@ -87,8 +99,7 @@ def read_csv_data(filepath, search_term):
     return results
 
 
-@app.route('/login', methods=['GET', 'POST'])
-
+@app.route("/login", methods=["GET", "POST"])
 def login():
 
     form = LoginForm()
@@ -101,50 +112,45 @@ def login():
 
         # Replace with actual user validation
 
-        if username == 'admin' and password == 'School12023+':
+        if username == "admin" and password == "School12023+":
 
             user = User(username)
 
             login_user(user)
 
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
 
 
-@app.route('/logout', methods=['POST'])
-
+@app.route("/logout", methods=["POST"])
 @login_required
-
 def logout():
 
     logout_user()
 
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
-@app.route('/', methods=['GET', 'POST'])
-
+@app.route("/", methods=["GET", "POST"])
 @login_required
-
 def index():
 
     results = []
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        username_or_pcname = request.form['search']
+        username_or_pcname = request.form["search"]
 
         # Replace the following path with the path to your file
 
-        filepath = '/mnt/share/UserLogons.csv'
+        filepath = "/mnt/share/UserLogons.csv"
 
         results = read_csv_data(filepath, username_or_pcname)
 
     return render_template("index.html", results=results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
